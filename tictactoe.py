6ae66d1141659c644a7388f173206b54787229d3
Renée Rodrigues <renee.rodrigues@edge.ufal.br>
2022-10-22 23:44:50 -0300

    Game restart

--- tictactoe.py
+++ tictactoe.py
@@ -8,14 +8,16 @@ def choose_player():
 
     validFunctions = ['q', 'r', 'x', 'o']
     validOptions   = range(1, 10)
-    player = ""
-    roundPlayer = 1
+    player         = ""
+    roundPlayer    = 1
     listPositions  = {1:" ", 2:" ", 3:" ", 4:" ", 5:" ", 6:" ", 7:" ", 8:" ", 9:" "}
-    win = False
         
     while player not in validFunctions:
 
-        print("\nPress Q anytime to quit the game...")
+        # Clear the screen
+        os.system('cls')
+
+        print("\nPress Q anytime to quit the game...\n")
 
         # Ask if the user want to be the X or the O
         player = input("Do you want to start as X or O? ")
@@ -25,19 +27,24 @@ def choose_player():
 
         # Ending the game
         if player.lower() == 'q':
-            print("Thank you for comming. See ya!")
-            break 
+
+            quit_game()
 
         # Wrong option selected
         if player not in validFunctions:
-            print("Wrong option selected. Try again")
+
+            print("\nWrong option selected. Try again\n")
+
             continue
         
         # Identifing chosen option
         if player.lower() == 'x':
+
             print("Starting game with X\n")
             roundPlayer = "X"
+            
         elif player.lower() == 'o':
+
             print("Starting game with O\n")
             roundPlayer = "O"
 
@@ -48,6 +55,7 @@ def choose_player():
                         4:"-----------------------",
                         5:"   7   |   8   |   9   "
                        }
+
         for line in defaultBoard:
             print(defaultBoard[line])
 
@@ -62,47 +70,29 @@ def choose_player():
 
                 # Check the end of game
                 count = 0
+
                 for pos in listPositions:
-                    count += 1 if pos == "X" or pos == "O" else 0
+
+                    count += 1 if listPositions[pos] == "X" or listPositions[pos] == "O" else 0
 
                 if count == 9:
+
                     print("Game Over!")
-                    listPositions  = {1:" ", 2:" ", 3:" ", 4:" ", 5:" ", 6:" ", 7:" ", 8:" ", 9:" "}
+                    listPositions = restart_game()
+                    choose_player()
+
                     continue
 
-                # Win check
-                if listPositions[1] == listPositions[2] and listPositions[1] == listPositions[3] and listPositions[1] != " ":
-                    print("Player "+listPositions[1]+" Won!")
-                    win = True
-                if listPositions[4] == listPositions[5] and listPositions[4] == listPositions[6] and listPositions[4] != " ":
-                    print("Player "+listPositions[4]+" Won!")
-                    win = True
-                if listPositions[7] == listPositions[8] and listPositions[7] == listPositions[9] and listPositions[7] != " ":
-                    print("Player "+listPositions[7]+" Won!")
-                    win = True
-                if listPositions[1] == listPositions[4] and listPositions[1] == listPositions[7] and listPositions[1] != " ":
-                    print("Player "+listPositions[1]+" Won!")
-                    win = True
-                if listPositions[2] == listPositions[5] and listPositions[2] == listPositions[8] and listPositions[2] != " ":
-                    print("Player "+listPositions[2]+" Won!")
-                    win = True
-                if listPositions[3] == listPositions[6] and listPositions[3] == listPositions[9] and listPositions[3] != " ":
-                    print("Player "+listPositions[3]+" Won!")
-                    win = True
-                if listPositions[1] == listPositions[5] and listPositions[1] == listPositions[9] and listPositions[1] != " ":
-                    print("Player "+listPositions[1]+" Won!")
-                    win = True
-                if listPositions[7] == listPositions[5] and listPositions[7] == listPositions[3] and listPositions[7] != " ":
-                    print("Player "+listPositions[7]+" Won!")
-                    win = True
+                # Winner check
+                if check_winner(listPositions):
 
-                if win:
-                    listPositions  = {1:" ", 2:" ", 3:" ", 4:" ", 5:" ", 6:" ", 7:" ", 8:" ", 9:" "}
-                    win = False
+                    listPositions = restart_game()
+                    choose_player()
 
-                    # Choose an option 1-9
-                    selectedOption = input("Press 'R' to restart the game! ")
+                    continue
+
                 else:
+
                     # Inform who's the next player
                     print("It's the "+roundPlayer+" turn")
 
@@ -116,31 +106,39 @@ def choose_player():
 
                 # Ending the game
                 if selectedOption.lower() == 'q':
-                    # Clear the screen
-                    os.system('cls')
 
-                    print("Thank you for comming. See ya!")
-                    break
+                    quit_game()
 
                 if selectedOption.lower() == 'r':
-                    # Clear the screen
-                    os.system('cls')
+
+                    listPositions = restart_game()
 
                     choose_player()
-                    break
 
-                # Parse to int
-                selectedOption = int(selectedOption)
-
-                # Check a valid option selected
-                if selectedOption not in validOptions:
-                    print("\nWrong option selected! Choose a number between 1-9")
                     continue
 
-                if (listPositions[selectedOption] != " "):
-                    print("\nOption already taken! Please choose another unchosen option")
-                    pass
+                # Check a valid option selected
+                if (not selectedOption.isdigit()) or (int(selectedOption) not in validOptions):
+                    
+                    print("\nWrong option selected! Choose a number between 1-9\n")
+                    
+                    # Printing the board
+                    print_board(listPositions)
+                    
+                    continue
+
                 else:
+                    # Parse to int
+                    selectedOption = int(selectedOption)
+
+                if (listPositions[selectedOption] != " "):
+
+                    print("\nOption already taken! Please choose another unchosen option")
+                    
+                    pass
+
+                else:
+
                     # Selected positions
                     listPositions[selectedOption] = roundPlayer
 
@@ -168,6 +166,57 @@ def print_board(listPositions):
         print(printBoard[line])
 
     print("\n")
+
+def check_winner(listPositions):
+    
+    win = False
+
+    # Win check
+    if listPositions[1] == listPositions[2] and listPositions[1] == listPositions[3] and listPositions[1] != " ":
+        print("Player "+listPositions[1]+" Won!")
+        win = True
+    if listPositions[4] == listPositions[5] and listPositions[4] == listPositions[6] and listPositions[4] != " ":
+        print("Player "+listPositions[4]+" Won!")
+        win = True
+    if listPositions[7] == listPositions[8] and listPositions[7] == listPositions[9] and listPositions[7] != " ":
+        print("Player "+listPositions[7]+" Won!")
+        win = True
+    if listPositions[1] == listPositions[4] and listPositions[1] == listPositions[7] and listPositions[1] != " ":
+        print("Player "+listPositions[1]+" Won!")
+        win = True
+    if listPositions[2] == listPositions[5] and listPositions[2] == listPositions[8] and listPositions[2] != " ":
+        print("Player "+listPositions[2]+" Won!")
+        win = True
+    if listPositions[3] == listPositions[6] and listPositions[3] == listPositions[9] and listPositions[3] != " ":
+        print("Player "+listPositions[3]+" Won!")
+        win = True
+    if listPositions[1] == listPositions[5] and listPositions[1] == listPositions[9] and listPositions[1] != " ":
+        print("Player "+listPositions[1]+" Won!")
+        win = True
+    if listPositions[7] == listPositions[5] and listPositions[7] == listPositions[3] and listPositions[7] != " ":
+        print("Player "+listPositions[7]+" Won!")
+        win = True
+
+    return win
+
+def restart_game():
+
+    input("\nPress [ENTER] to play again! ")
+
+    # Clear the screen
+    os.system('cls')
+
+    return {1:" ", 2:" ", 3:" ", 4:" ", 5:" ", 6:" ", 7:" ", 8:" ", 9:" "}
+
+def quit_game():
+
+    # Clear the screen
+    os.system('cls')
+
+    print("Thank you for comming. See ya!")
+
+    quit() 
+    
     
 # Starting the game
 choose_player()
